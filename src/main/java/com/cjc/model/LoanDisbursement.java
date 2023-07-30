35a7a70f7f44a31edbb80dd7377b5d97266211c8
PradipPatil <pradippatil8063@gmail.com>
2023-07-30 16:41:01 +0530

    add annotation to pojo class

--- src/main/java/com/cjc/model/LoanDisbursement.java
+++ src/main/java/com/cjc/model/LoanDisbursement.java
@@ -5,16 +5,16 @@ import lombok.Data;
 @Data
 public class LoanDisbursement {
 
-	int agreemenId;
-	int loanNo;
-	String agrrementDate;
-	String amountPayTime;
-	Double totalAmount;
-	String bankName;
-	Double accountNumber;
-	String IFSCCode;
-	String accountType;
-	double transferAmount;
-	String paymentStatus;
-	String amountPaidDate;
+	private	int agreemenId;
+	private	int loanNo;
+	private	String agrrementDate;
+	private	String amountPayTime;
+	private	Double totalAmount;
+	private	String bankName;
+	private	Double accountNumber;
+	private	String IFSCCode;
+	private	String accountType;
+	private	double transferAmount;
+	private	String paymentStatus;
+	private	String amountPaidDate;
 }
