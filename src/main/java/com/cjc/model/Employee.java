ba8d1c92b100e93373ebf81a555acfbf012c83de
RahulNagawade12 <nagawaderahul0@gmail.com>
2023-07-31 12:19:59 +0530

    Add pojo classes

---
+++ src/main/java/com/cjc/model/Employee.java
@@ -0,0 +1,29 @@
+package com.cjc.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Builder
+public class Employee {
+	@Id
+	private int empid;
+	private String name;
+	private String designation;
+	private double salary;
+	private String username;
+	private String password;
+	private String status;
+	@OneToOne
+	private EmployeeAddress address;
+
+}
